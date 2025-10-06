8bae69fabb032ca1e5c834fc801d6750132c2863
CodeAwareness <codeawareness@gmail.com>
2025-10-05 17:31:56 -0700

    doom emacs

---
+++ doom/custom.el
@@ -0,0 +1,18 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8"
+     "220e0a7b5a0d8f155432a5e9b24d33e0f7bc56fdeb2ec5fcc0b78453305efbb6"
+     "fd22a3aac273624858a4184079b7134fb4e97104d1627cb2b488821be765ff17"
+     "0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1"
+     "8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
